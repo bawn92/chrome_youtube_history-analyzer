96c5ab9d0f82ff0fa955cd5fe11b3434167ca7d5
bawn92 <gearoidmoc@gmail.com>
2016-01-13 23:00:46 +0000

    Preparing objects for HTML5 local storage

--- content_script.js
+++ content_script.js
@@ -4,6 +4,7 @@ var videoIDs = new Array();
 var count = 0;
 var count2 =  0;
 var time_watched = "00:00";
+var videos_map ={};
 
 // Regex for YouTube links and YouTube IDs
 var hrefLink = new RegExp("href=\"/watch\\?v=.[^\"]{0,40}");
@@ -42,18 +43,31 @@ function clickPlanet() {
 
 function doSearch(searchString, details)
 {
-
+		Object.size = function(obj) {
+				var size = 0, key;
+				for (key in obj) {
+						if (obj.hasOwnProperty(key)) size++;
+				}
+				return size;
+		};
 		//search for title name, link and video length
 		while(searchString.match('<h3.{100,}</h3>')[0]){
 
 				var h3tagPostion = searchString.search('<h3.{100,}</h3>');
 				var h3tag = searchString.match('<h3.{100,}</h3>')[0];
+				var channelName;
+				var channelLink;
+				var videoUrl;
+				var videoDuration;
+				var video_title;
 
 				//verify h3 tags contain information we need to extract
 				if(h3tag.search(titleName) != -1 && h3tag.search(hrefLink) != -1 && h3tag.search(duration) != -1){
 					count = count+1;
-					console.log(count);
 					time_watched = timeAdd(time_watched,cleanDuration(h3tag.match(duration)[0]));
+					videoUrl = h3tag.match(hrefLink)[0];
+					videoDuration = cleanDuration(h3tag.match(duration)[0]);
+					video_title = h3tag.match(titleName)[0];
 
 				}else {
 					count2 = count2+1;
@@ -64,11 +78,12 @@ function doSearch(searchString, details)
 					var channeldetails = searchString.match('<div class="yt-lockup-byline">.{0,}</div>')[0];
 					if(channeldetails != null){
 
-						//alert(cleanChannelLink(channeldetails.match(channelLink)));
-						alert(cleanChannelName(channeldetails.match(channelName)[0]));
-
+						channelLink = channeldetails.match(channelLink)[0];
+						channelName = cleanChannelName(channeldetails.match(channelName)[0]);
 					}
 
+					addVideo(videoUrl,channelLink,videoDuration,video_title);
+					//verifyFirst50Videos();
 
 				searchString = searchString.substring(channeldetailsPostion+channeldetails.length,searchString.length);
 				if(!searchString.match('<h3.{100,}</h3>')){
@@ -80,7 +95,31 @@ function doSearch(searchString, details)
 
 
 		console.log("exit");
+		console.log(Object.size(videos_map));
 		alert(" Number Of videos watched: "+count+" Hours watched: "+time_watched);
+
+}
+
+
+function addVideo(videoUrl,channelLink,videoDuration,video_title)
+{
+
+	console.log(channelLink);
+	console.log(videoUrl);
+	 if(!(channelLink in videos_map)){
+		 chan_map = [{videoUrl:{"Length":videoDuration, "Title":video_title}}];
+		 videos_map[channelLink] = chan_map;
+		 videos_map.channelLink = chan_map;
+		console.log(videos_map);
+	 }else{
+		 chan_vids = videos_map[channelLink];
+		 console.log(chan_vids.type);
+		 var video = {"Length":videoDuration, "Title":video_title};
+		 chan_vids.push({videoUrl:video});
+		 videos_map[channelLink] = chan_vids.push({videoUrl:video});
+	 }
+
+
 
 }
 
@@ -93,7 +132,7 @@ function timeAdd(s1,s2)
   return (v[0]+Math.floor(v[1]/60))+':'+v[1]%60;
 }
 
-
+//Cleans duration
 function cleanDuration(duration)
 {
 
@@ -113,6 +152,9 @@ function cleanDuration(duration)
 		return duration;
 
 }
+
+
+
 
 function cleanChannelName(name)
 {
