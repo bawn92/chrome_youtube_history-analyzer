c825aead0d241b9e09c37bb92c78ee2881ce22fd
bawn92 <gearoidmoc@gmail.com>
2016-01-15 18:13:34 +0000

    Updated popup css & channel printing

--- content_script.js
+++ content_script.js
@@ -5,6 +5,8 @@ var count = 0;
 var count2 =  0;
 var time_watched = "00:00";
 var videos_map ={};
+var channel_name_link = {};
+
 
 // Regex for YouTube links and YouTube IDs
 var hrefLink = new RegExp("href=\"/watch\\?v=.[^\"]{0,40}");
@@ -21,21 +23,31 @@ clickPlanet();
 
 
 function clickPlanet() {
-
-	var checker = function(){
+		var num = 0;
+		var checker = function(){
 		number = document.getElementsByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more browse-items-load-more-button").length;
 		if( number == 1){
+					num = num +1;
 					document.getElementsByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more browse-items-load-more-button")[0].click();
+					var pictures = document.getElementsByClassName("yt-lockup-thumbnail contains-addto");
+					if( num % 50 == 0 || num == 1){
+						for (var i = 0; i < pictures.length; i ++) {
+	    				pictures[i].style.display = "none";
+						}
+					}
 		}else if (number == 0){
 			//once end of page reached search
 			clearInterval(timer);
 			alert("starting search");
+			var pictures = document.getElementsByClassName("yt-lockup-thumbnail contains-addto");
+			for (var i = 0; i < pictures.length; i ++) {
+				pictures[i].style.display = "none";
+			}
 			doSearch(document.body.innerHTML,'');
 			alert("Finished");
 
 		}
 	};
-
 	timer = setInterval(checker,1000);
 
 }
@@ -80,6 +92,7 @@ function doSearch(searchString, details)
 
 						channel_Link = channeldetails.match(channelLink)[0];
 						channel_Name = cleanChannelName(channeldetails.match(channelName)[0]);
+						channel_name_link[channel_Link] = channel_Name;
 					}
 
 					addVideo(videoUrl,channel_Link,videoDuration,video_title);
@@ -87,7 +100,6 @@ function doSearch(searchString, details)
 
 				searchString = searchString.substring(channeldetailsPostion+channeldetails.length,searchString.length);
 				if(!searchString.match('<h3.{100,}</h3>')){
-					console.log("in break");
 					break;
 				}
 		}
@@ -95,8 +107,6 @@ function doSearch(searchString, details)
 		find_most_viewed_channels();
 
 		console.log("exit");
-		console.log(Object.size(videos_map));
-		console.log(videos_map);
 		alert(" Number Of videos watched: "+count+" Hours watched: "+time_watched);
 
 }
@@ -105,18 +115,13 @@ function doSearch(searchString, details)
 function addVideo(videoUrl,channel_Link,videoDuration,video_title)
 {
 
-	console.log(channel_Link);
-	console.log(videoUrl);
 	 if(!(channel_Link in videos_map)){
 		 chan_map = [{videoUrl:{"Length":videoDuration, "Title":video_title}}];
 		 videos_map[channel_Link] = chan_map;
 		 //videos_map.channel_Link = chan_map;
-		 console.log(videos_map);
 	 }else{
 		 chan_vids = videos_map[channel_Link];
-		 console.log(chan_vids);
 		 chan_vids.push({videoUrl:{"Length":videoDuration, "Title":video_title}});
-		 console.log(chan_vids);
 		 videos_map[channel_Link] = chan_vids;
 	 }
 
@@ -130,8 +135,21 @@ function find_most_viewed_channels()
 		for(var video in videos_map)
 			sortable.push([video,videos_map[video].length]);
 		sortable.sort(function(a,b) {return b[1] - a[1]})
-		console.log("sortable");
-		console.log(sortable);
+
+		/*
+		for(var link in sortable){
+			//console.log(channel_name_link[link[0]]+" Videos watched"+link[1]);
+			console.log(link);
+			console.log(link[0]);
+			console.log(link[1]);
+		}*/
+
+		for (var i = 0; i < sortable.length; i++) {
+				if(sortable[i][1] >10){
+					console.log(channel_name_link[sortable[i][0]]+" channel videos watched "+sortable[i][1]);
+				}
+		}
+
 }
 
 
@@ -157,9 +175,6 @@ function cleanDuration(duration)
 		}else if (duration.length == 2){
 			duration = "00:".concat(duration);
 		}
-
-
-		console.log(duration);
 		return duration;
 
 }
